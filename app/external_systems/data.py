1ed4b5a9d13709969d0ae34fbb911579f0ec3ed5
John Tamayo <tamayonauta@gmail.com>
2020-01-22 17:23:13 -0500

    🌱 (external_systems): Add more persons

--- app/external_systems/data.py
+++ app/external_systems/data.py
@@ -6,15 +6,33 @@ PERSONAL_DATA = [
         "full_name": "John Doe"
     },
     {
+        "id_type": "CC",
+        "id_number": "1000001",
+        "id_exp_date": "2001-01-12",
+        "full_name": "Diana Doe"
+    },
+    {
         "id_type": "CE",
         "id_number": "2000000",
-        "id_exp_date": "2002-02-12",
+        "id_exp_date": "2002-02-13",
         "full_name": "Ana Smith"
+    },
+    {
+        "id_type": "CE",
+        "id_number": "2000001",
+        "id_exp_date": "2002-02-14",
+        "full_name": "Andrew Smith"
     },
     {
         "id_type": "PP",
         "id_number": "3000000",
-        "id_exp_date": "2003-03-13",
+        "id_exp_date": "2003-03-15",
         "full_name": "Bill Kent"
+    },
+    {
+        "id_type": "PP",
+        "id_number": "3000001",
+        "id_exp_date": "2003-03-16",
+        "full_name": "Marge Kent"
     }
 ]
